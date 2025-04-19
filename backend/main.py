6c072319de15ccb83d615c2959769ade40172120
Ayşegül Yıldız <99360744+aysegul-yildiz@users.noreply.github.com>
2025-04-20 00:31:51 +0300

    moved  main to backend and implement /predict route

---
+++ backend/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from app.text_similarity import create_faiss_index, calculate_similarity
+
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastAPI!"}
+
+#for now mock data
+PAST_ISSUES = [
+    "App crashes on profile click",
+    "Login fails with wrong password",
+    "Image upload silently fails",
+    "App freezes on login",
+    "Unable to reset password on mobile",
+    "Clicking save does nothing in profile settings"
+]
+
+class Query(BaseModel):
+    text: str
+
+@app.post("/predict")
+def predict(query: Query):
+    faiss_index, model = create_faiss_index(PAST_ISSUES)
+    similar_issue, score = calculate_similarity(faiss_index, model, PAST_ISSUES, query.text)
+    return {
+        "query": query.text,
+        "most_similar": similar_issue,
+        "similarity_score": score
+    }
